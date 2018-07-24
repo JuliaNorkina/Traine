4ec6eda0ca125856c0ddb6575402cfb3e0162b50
YuliaNorkina <yulia.norkina2@gmail.com>
2018-07-24 17:55:27 +0300

    create two activity and complete task#1

--- simpleapp/src/main/java/com/example/simpleapp/FirstActivity.java
+++ simpleapp/src/main/java/com/example/simpleapp/FirstActivity.java
@@ -1,13 +1,39 @@
 package com.example.simpleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvInfo;
+    Button bEnterData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first);
+
+        tvInfo = findViewById(R.id.tvInfo);
+        bEnterData = findViewById(R.id.bEnterData);
+
+        bEnterData.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) return;
+        String string = "FIO: " + data.getStringExtra("fio") + ", age: " + data.getStringExtra("age");
+        tvInfo.setText(string);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- simpleapp/src/main/java/com/example/simpleapp/SecondActivity.java
+++ simpleapp/src/main/java/com/example/simpleapp/SecondActivity.java
@@ -1,13 +1,35 @@
 package com.example.simpleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etFio, etAge;
+    Button bOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
+
+        etFio = findViewById(R.id.etFio);
+        etAge = findViewById(R.id.etAge);
+        bOk = findViewById(R.id.bOk);
+
+        bOk.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.putExtra("fio", etFio.getText().toString());
+        intent.putExtra("age", etAge.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
